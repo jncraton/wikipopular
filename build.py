c3cc343754daec4c89825b0b46e3512e9407a20e
Jon Craton <jncraton@gmail.com>
2018-12-05 14:38:14 -0500

    multiprocessing

--- build.py
+++ build.py
@@ -4,46 +4,60 @@ import json
 from time import perf_counter
 from bs4 import BeautifulSoup
 
+def parse_pop_page(pop_page):
+  url = 'https://en.wikipedia.org/w/api.php?action=parse&page=%s&format=json&prop=text&redirects=1' % quote_plus(pop_page)
+  f = urllib.request.urlopen(url)
+  res = json.loads(f.read())
+
+  html = res['parse']['text']['*']
+
+  soup = BeautifulSoup(html,features='html.parser')
+
+  pages = ""
+
+  try:
+    for row in soup.find('table',class_='wikitable').find('tbody').find_all('tr'):
+      cells = row.find_all('td')
+      if len(cells) < 2: continue
+
+      try:
+        href = cells[1].find('a')['href']
+      except:
+        # We end up here if the link doesn't exist. Some of the pages are buggy, and we just skip that entry
+        continue
+      try:
+        views = cells[2].find('a').get_text()
+      except AttributeError:
+        views = cells[2].get_text()
+
+      views = int(views.replace(',', ''))
+
+      if not '&redlink=1' in href and views > 10000:
+        page = unquote_plus(href[6:])
+        pages += "%d\t%s\n" % (views, page)
+  except (AttributeError, TypeError) as e:
+    print(e)
+    print(link)
+    exit(1)
+
+  return pages
+
 def get_cur_pop():
   index = 'https://en.wikipedia.org/w/api.php?action=parse&page=User:Community_Tech_bot/Popular_pages&format=json&prop=links&redirects=1'
   
   f = urllib.request.urlopen(index)
   index = json.loads(f.read())
   
-  for link in index['parse']['links']:
-    link = link['*']
-    if '/Popular pages' in link:
-      page = 'https://en.wikipedia.org/w/api.php?action=parse&page=%s&format=json&prop=text&redirects=1' % quote_plus(link)
-      f = urllib.request.urlopen(page)
-      res = json.loads(f.read())
+  pages = [link['*'] for link in index['parse']['links'] if '/Popular pages' in link['*']]
 
-      html = res['parse']['text']['*']
+  print(pages)
 
-      soup = BeautifulSoup(html,features='html.parser')
+  import multiprocessing
 
-      try:
-        for row in soup.find('table',class_='wikitable').find('tbody').find_all('tr'):
-          cells = row.find_all('td')
-          if len(cells) < 2: continue
+  pool = multiprocessing.Pool()
 
-          try:
-            href = cells[1].find('a')['href']
-          except:
-            # We end up here if the link doesn't exist. Some of the pages are buggy, and we just skip that entry
-            continue
-          try:
-            views = cells[2].find('a').get_text()
-          except AttributeError:
-            views = cells[2].get_text()
-  
-          views = int(views.replace(',', ''))
+  pages = pool.map(parse_pop_page, pages)
 
-          if not '&redlink=1' in href and views > 10000:
-            page = unquote_plus(href[6:])
-            print("%d\t%s" % (views, page))
-      except (AttributeError, TypeError) as e:
-        print(e)
-        print(link)
-        exit(1)
+  print(''.join(pages))
 
 get_cur_pop()
